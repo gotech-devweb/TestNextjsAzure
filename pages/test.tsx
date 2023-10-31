4a7448bd8908e62091337697007f0acb3e4bf2c3
Sang Ha <haphuocsang1801@gmail.com>
2023-10-31 14:54:56 +0700

    test deploy

---
+++ pages/test.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Test = () => {
+  return (
+    <>
+      {/* //create for me a static web */}
+      <h1>Hello World</h1>
+      <p>This is my first Next.js app</p>
+    </>
+  );
+};
+
+export default Test;
